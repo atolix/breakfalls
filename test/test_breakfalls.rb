6c2f776687224599f1ab34a158ce9968a321b58b
atolix <82761106+atolix@users.noreply.github.com>
2025-09-18 21:00:24 +0900

    core test

--- test/test_breakfalls.rb
+++ test/test_breakfalls.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TestBreakfalls < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Breakfalls::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_error_handler_called
+    called = false
+    Breakfalls.on_error { |_e, _req, _user, _params| called = true }
+    Breakfalls.run_error_handlers(StandardError.new('error!'))
+    assert called, 'error handler should be called'
   end
 end
