76f05671ff6dd27472744c6d42dc9ab97ac7d613
atolix <82761106+atolix@users.noreply.github.com>
2025-09-18 22:25:24 +0900

    on load

--- lib/breakfalls/railtie.rb
+++ lib/breakfalls/railtie.rb
@@ -7,28 +7,30 @@ module Breakfalls
     config.breakfalls = ActiveSupport::OrderedOptions.new
 
     config.to_prepare do
-      Array(config.breakfalls.controllers)
+      controllers = Array(Rails.application.config.breakfalls.controllers)
       next if controllers.empty?
 
       controllers.each do |controller|
-        klass = controller.safe_constantize
-        next unless klass
+        ActiveSupport.on_load(:action_controller) do
+          klass = controller.safe_constantize
+          next unless klass
 
-        klass.class_eval do
-          around_action :breakfalls_dispatch
+          klass.class_eval do
+            around_action :breakfalls_dispatch
 
-          private
+            private
 
-          def breakfalls_dispatch
-            yield
-          rescue StandardError => e
-            Breakfalls.run_error_handlers(
-              e,
-              request,
-              (respond_to?(current_user) ? current_user : nil),
-              params
-            )
-            raise e
+            def breakfalls_dispatch
+              yield
+            rescue StandardError => e
+              Breakfalls.run_error_handlers(
+                e,
+                request,
+                (respond_to?(current_user) ? current_user : nil),
+                params
+              )
+              raise e
+            end
           end
         end
       end
