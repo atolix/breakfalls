ab196d73b367e6e2f26e21e18104c25f20916a99
atolix <82761106+atolix@users.noreply.github.com>
2025-09-21 18:04:46 +0900

    chore: add test for another controller and unregisted controller

--- test/test_breakfalls_integration.rb
+++ test/test_breakfalls_integration.rb
@@ -25,6 +25,18 @@ class DummyController < ActionController::Base
   end
 end
 
+class AnotherController < ActionController::Base
+  def unrescued_standard
+    raise StandardError, 'another error!'
+  end
+end
+
+class UnregisteredController < ActionController::Base
+  def unrescued_standard
+    raise StandardError, 'unregistered error!'
+  end
+end
+
 class DummyRailsApp < Rails::Application
   config.secret_key_base = 'dummy'
   config.active_support.to_time_preserves_timezone = :zone
@@ -34,13 +46,15 @@ class DummyRailsApp < Rails::Application
     get '/unrescued_standard' => 'dummy#unrescued_standard'
     get '/rescued_custom' => 'dummy#rescued_custom'
     get '/rescued_standard' => 'dummy#rescued_standard'
+    get '/another_unrescued_standard' => 'another#unrescued_standard'
+    get '/unregistered_unrescued_standard' => 'unregistered#unrescued_standard'
   end
 end
 
 class BreakfallsIntegrationTest < ActionDispatch::IntegrationTest
   def setup
     Rails.application = DummyRailsApp.instance
-    Rails.application.config.breakfalls.controllers = %w[DummyController]
+    Rails.application.config.breakfalls.controllers = %w[DummyController AnotherController]
     Rails.application.initialize! unless Rails.application.initialized?
     Rails.application.reloader.prepare!
   end
@@ -72,5 +86,28 @@ class BreakfallsIntegrationTest < ActionDispatch::IntegrationTest
     get '/rescued_standard'
   ensure
     assert !called, 'Breakfalls handler should not be called when StandardError is rescued in the controller'
+  end
+
+  def test_handler_called_on_unrescued_standard_error_in_another_controller
+    called = false
+    Breakfalls.on_error { |_e, _req, _user, _params| called = true }
+
+    get '/another_unrescued_standard'
+  rescue StandardError
+    # skip exception
+  ensure
+    assert called, 'Breakfalls handler should be called for another registered controller when it raises'
+  end
+
+  def test_handler_not_called_for_unregistered_controller
+    called = false
+    Breakfalls.on_error { |_e, _req, _user, _params| called = true }
+
+    get '/unregistered_unrescued_standard'
+  rescue StandardError
+    # skip exception
+  ensure
+    assert !called,
+           'Breakfalls handler should not be called for controllers not registered in config.breakfalls.controllers'
   end
 end
